7a2e31f5d11f41b9623aa77d378460f191ff9eb7
Dhruv-Ku12 <dmontage000@gmail.com>
2025-06-10 22:16:23 +0530

    initial files

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import App from "./App";
+
+const index = () => {
+  return <App />;
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVnPYNmjKGai2ei3rm1zE3khr2uDkKKbE",
+  authDomain: "truemart-eae2f.firebaseapp.com",
+  projectId: "truemart-eae2f",
+  storageBucket: "truemart-eae2f.firebasestorage.app",
+  messagingSenderId: "704916906825",
+  appId: "1:704916906825:web:6bbf4c7d56e5fe8c547098",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
